d0e2e01c02ec01c34f63d073ea13c751bee945d2
Chistian Kern <ikerneii97@gmail.com>
2019-10-12 22:56:46 +0200

    Implemnted Delete Server

--- minecraft/View/Xaml/SettingsWindow.xaml.cs
+++ minecraft/View/Xaml/SettingsWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿
 using System.Windows;
+using nihilus.Logic.Manager;
 using nihilus.ViewModel;
 
 namespace nihilus.View.Xaml
@@ -22,6 +23,31 @@ namespace nihilus.View.Xaml
         {
             Close();
             viewModel.UpdateSettings();
+        }
+
+        private void Btn_Delete(object sender, RoutedEventArgs e)
+        {
+            Close();
+            ApplicationManager.Instance.MainViewModel.Servers.Remove(viewModel);
+            
+            /*
+             * TODO: save world in zip
+             */
+        }
+
+        private void Btn_ChangeVersion(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void Btn_RegenerateEnd(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void Btn_RegenerateNether(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
