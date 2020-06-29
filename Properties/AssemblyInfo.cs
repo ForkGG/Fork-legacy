93d60f0db6e643b15e78c294942d3f34f8d8a7b3
Christian Kern <kernchri@fim.uni-passau.de>
2020-06-29 13:11:01 +0200

    Updated files for 0.7.1 release

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Fork")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Fork is a Minecraft Server Manager GUI making server creation, configuration and maintenance as simple as possible")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Fork")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.7.0.0")]
-[assembly: AssemblyFileVersion("0.7.0.0")]
+[assembly: AssemblyVersion("0.7.1.0")]
+[assembly: AssemblyFileVersion("0.7.1.0")]
