ee17fe255a4fa2c36019b8b0d6dea0641cf599f5
Newt Welch <42378068+newtwelch@users.noreply.github.com>
2021-01-06 10:31:16 -0500

    Newt (#18)
    * Query Port, a few fixes
    
    * Query Port
    
    * fixed stupid online mode turning into online m ode

--- logic/model/Server.cs
+++ logic/model/Server.cs
@@ -14,7 +14,7 @@ namespace Fork.Logic.Model
     public class Server : Entity
     {
         private ServerSettings serverSettings;
-        
+
         public string UID { get; set; }
         public string Name { get; set; }
         public ServerVersion Version { get; set; }
@@ -29,17 +29,18 @@ namespace Fork.Logic.Model
         public RestartTime Restart2 { get; set; }
         public RestartTime Restart3 { get; set; }
         public RestartTime Restart4 { get; set; }
-        public StopTime AutoStop1 { get; set; } = new(false,new SimpleTime(0,0));
-        public StopTime AutoStop2 { get; set; } = new(false,new SimpleTime(12,0));
-        public StartTime AutoStart1 { get; set; } = new(false,new SimpleTime(0,0));
-        public StartTime AutoStart2 { get; set; } = new(false,new SimpleTime(12,0));
-        
+        public StopTime AutoStop1 { get; set; } = new(false, new SimpleTime(0, 0));
+        public StopTime AutoStop2 { get; set; } = new(false, new SimpleTime(12, 0));
+        public StartTime AutoStart1 { get; set; } = new(false, new SimpleTime(0, 0));
+        public StartTime AutoStart2 { get; set; } = new(false, new SimpleTime(12, 0));
+
         [JsonIgnore]
-        public ServerSettings ServerSettings {
+        public ServerSettings ServerSettings
+        {
             get
             {
                 if (serverSettings == null)
-                    serverSettings = new ServerSettings(new FileReader().ReadServerSettings(Path.Combine(App.ServerPath,Name)));
+                    serverSettings = new ServerSettings(new FileReader().ReadServerSettings(Path.Combine(App.ServerPath, Name)));
                 return serverSettings;
             }
             set => serverSettings = value;
@@ -51,31 +52,32 @@ namespace Fork.Logic.Model
         [JsonIgnore]
         public string JarLink => Version.JarLink;
 
-        public Server(String name, ServerVersion version, ServerSettings serverSettings, JavaSettings javaSettings) {
+        public Server(String name, ServerVersion version, ServerSettings serverSettings, JavaSettings javaSettings)
+        {
             Name = name;
             Version = version;
             JavaSettings = javaSettings;
             ServerSettings = serverSettings;
-            Restart1 = new(false,new SimpleTime(0,0));
-            Restart2 = new(false,new SimpleTime(6,0));
-            Restart3 = new(false,new SimpleTime(12,0));
-            Restart4 = new(false,new SimpleTime(18,0));
-            AutoStop1 = new(false,new SimpleTime(0,0));
-            AutoStop2 = new(false,new SimpleTime(12,0));
-            AutoStart1 = new(false,new SimpleTime(0,0));
-            AutoStart2 = new(false,new SimpleTime(12,0));
+            Restart1 = new(false, new SimpleTime(0, 0));
+            Restart2 = new(false, new SimpleTime(6, 0));
+            Restart3 = new(false, new SimpleTime(12, 0));
+            Restart4 = new(false, new SimpleTime(18, 0));
+            AutoStop1 = new(false, new SimpleTime(0, 0));
+            AutoStop2 = new(false, new SimpleTime(12, 0));
+            AutoStart1 = new(false, new SimpleTime(0, 0));
+            AutoStart2 = new(false, new SimpleTime(12, 0));
             UID = Guid.NewGuid().ToString("D");
         }
 
         /// <summary>
         /// Constructor for deserializer
         /// </summary>
-        public Server() {}
+        public Server() { }
 
         [OnDeserialized]
         internal void EnsureUid(StreamingContext context)
         {
-            if (UID == null ||UID.Equals(""))
+            if (UID == null || UID.Equals(""))
             {
                 UID = Guid.NewGuid().ToString("D");
             }
@@ -84,11 +86,11 @@ namespace Fork.Logic.Model
         public override string ToString()
         {
             string name = Name;
-            if (Name.Length>10)
+            if (Name.Length > 10)
             {
                 name = name.Substring(0, 10);
             }
-            return name + " ("+Version.Version+")";
+            return name + " (" + Version.Version + ")";
         }
     }
 }
