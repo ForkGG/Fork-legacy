6facaa7b34cc298c0a9cdfffb9867d8abcc858f0
ChristianKern97 <ikerneii97@gmail.com>
2021-01-30 00:47:57 +0100

    Added caching to ForkAPI calls

---
+++ logic/model/APIModels/CachedAPIResponse.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Timers;
+using Fork.Logic.Controller;
+
+namespace Fork.Logic.Model.APIModels
+{
+    public class CachedAPIResponse
+    {
+        private Timer timer;
+            
+        public HttpWebResponse WebResponse { get; }
+        public TimeSpan MaxCacheAge { get; }
+
+        public CachedAPIResponse(string requestHash, HttpWebResponse webResponse, TimeSpan maxCacheAge)
+        {
+            WebResponse = webResponse;
+            MaxCacheAge = maxCacheAge;
+
+            timer = new Timer
+            {
+                AutoReset = false,
+                Interval = maxCacheAge.TotalMilliseconds,
+                Enabled = true
+            };
+            timer.Elapsed += (_, _) =>
+            {
+                if (APIController.ResponseCache.ContainsKey(requestHash))
+                {
+                    APIController.ResponseCache.Remove(requestHash);
+                }
+                timer.Dispose();
+            };
+        }
+    }
+}
