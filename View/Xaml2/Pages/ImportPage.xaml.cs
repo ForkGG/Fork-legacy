457866682405960c391d5e3da917f543e2239fdf
KalmaDeb <karmaconfigs@gmail.com>
2023-12-31 00:51:49 +0100

    Added local plugins for imported servers

--- View/Xaml2/Pages/ImportPage.xaml.cs
+++ View/Xaml2/Pages/ImportPage.xaml.cs
@@ -1,14 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
+using Fork.logic.Utils;
 using Fork.Logic.ImportLogic;
 using Fork.Logic.Logging;
 using Fork.Logic.Manager;
 using Fork.Logic.Model;
+using Fork.Logic.Model.ServerConsole;
+using Fork.Logic.Utils;
+using Fork.View.Xaml2.Pages.Server;
 using Fork.ViewModel;
 using Application = System.Windows.Application;
 using Binding = System.Windows.Data.Binding;
@@ -40,6 +47,12 @@ namespace Fork.View.Xaml2.Pages
         private void ServerTypeSnapshot_Click(object sender, RoutedEventArgs e)
         {
             versionComboBox.SetBinding(ComboBox.ItemsSourceProperty, new Binding { Source = viewModel.SnapshotServerVersions });
+            versionComboBox.SelectedIndex = 0;
+        }
+
+        private void ServerTypeSpigot_Click(object sender, RoutedEventArgs e)
+        {
+            versionComboBox.SetBinding(ComboBox.ItemsSourceProperty, new Binding { Source = viewModel.SpigotVersions });
             versionComboBox.SelectedIndex = 0;
         }
 
@@ -76,6 +89,69 @@ namespace Fork.View.Xaml2.Pages
             ServerValidationInfo validationInfo = DirectoryValidator.ValidateServerDirectory(oldDir);
 
             bool createServerSuccess = await ServerManager.Instance.ImportServerAsync(selectedVersion, validationInfo, oldDir.FullName, serverName);
+            ServerViewModel viewModel = ApplicationManager.Instance.MainViewModel.SelectedEntity as ServerViewModel;
+
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Elapsed += async(e, a) =>
+            {
+                string serverPath = Path.Combine(App.ServerPath, serverName);
+                string pluginsFolder = Path.Combine(serverPath, "plugins");
+
+                if (Directory.Exists(pluginsFolder))
+                {
+                    viewModel.AddToConsole(
+                                new ConsoleMessage($"[LOCAL PLUGIN INSTALLATION] Loading plugins", ConsoleMessage.MessageLevel.INFO));
+
+                    string[] files = Directory.GetFiles(pluginsFolder);
+                    List<string> jarFiles = files
+                        .Where(archivo => Path.GetExtension(archivo).Equals(".jar", StringComparison.OrdinalIgnoreCase))
+                        .ToList();
+
+                    PluginsPage pluginsPage = viewModel.PluginsPage as PluginsPage;
+
+                    Application.Current.Dispatcher.Invoke(async() =>
+                    {
+                        foreach (string file in jarFiles)
+                        {
+                            string pluginName = LocalPluginUtils.ReadPluginName(file);
+                            string fileName = Path.GetFileName(file);
+                            string targetName = pluginName ?? StringUtils.BeautifyPluginName(fileName[..^4]);
+
+                            if (pluginName == null)
+                            {
+                                viewModel.AddToConsole(
+                                    new ConsoleMessage($"[LOCAL PLUGIN INSTALLATION] Possible non plugin file installed {fileName}", ConsoleMessage.MessageLevel.WARN));
+                            }
+
+                            bool added = await LocalPluginUtils.DoVirtualLoading(pluginsPage.viewModel, targetName, file, file);
+                            if (added)
+                            {
+                                viewModel.AddToConsole(
+                                    new ConsoleMessage($"[LOCAL PLUGIN INSTALLATION] Loaded plugin from imported server {targetName}",
+                                    ConsoleMessage.MessageLevel.SUCCESS));
+                            }
+                            else
+                            {
+                                viewModel.AddToConsole(
+                                    new ConsoleMessage($"[LOCAL PLUGIN INSTALLATION] Failed to load plugin from imported server {targetName}",
+                                    ConsoleMessage.MessageLevel.ERROR));
+                            }
+                        }
+                    });
+                }
+            };
+            timer.AutoReset = false;
+            timer.Interval = 1000;
+            timer.Start();
+            /*
+             * So, apparently, if we try to load the plugins right
+             * after the server has been imported, it messes up with
+             * server directory watchdog and crashes the application.
+             * This is the approach I've ended up with, it's not the
+             * best, and I should probably relay on some other kind of
+             * event, but for now it works
+             * - KarmaDev
+             */
 
             //TODO Do something if creating fails
         }
