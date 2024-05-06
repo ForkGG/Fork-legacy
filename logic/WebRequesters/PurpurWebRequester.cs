d55045c5c1cf56360cfbfb5f2b7dafe29a6a1121
ChristianKern97 <ikerneii97@gmail.com>
2024-05-06 20:28:47 +0200

    Switched Purpur web requester to call api directly

--- logic/WebRequesters/PurpurWebRequester.cs
+++ logic/WebRequesters/PurpurWebRequester.cs
@@ -14,7 +14,7 @@ public class PurpurWebRequester
 {
     public async Task<List<ServerVersion>> RequestPurpurVersions()
     {
-        string url = "https://serverjars.com/api/fetchAll/servers/purpur";
+        string url = "https://api.purpurmc.org/v2/purpur";
         string json = ResponseCache.Instance.UncacheResponse(url);
         if (json == null)
         {
@@ -24,15 +24,9 @@ public class PurpurWebRequester
                 HttpWebRequest request = WebRequest.CreateHttp(uri);
                 request.UserAgent = ApplicationManager.UserAgent;
                 using (WebResponse response = request.GetResponse())
-                using (Stream stream = response.GetResponseStream())
+                await using (Stream stream = response.GetResponseStream())
                 using (StreamReader reader = new(stream))
                     json = await reader.ReadToEndAsync();
-
-                dynamic fullResponse = JsonConvert.DeserializeObject(json);
-                if (!fullResponse.status.ToString().Equals("success"))
-                {
-                    throw new Exception("Invalid response from serverjars.com. No or wrong status found!");
-                }
 
                 ResponseCache.Instance.CacheResponse(url, json);
             }
@@ -40,30 +34,37 @@ public class PurpurWebRequester
             {
                 ErrorLogger.Append(e);
                 Console.WriteLine(
-                    "Could not receive Purpur Versions (either serverjars.com is down or your Internet connection is not working)");
+                    "Could not receive Purpur Versions (either api.purpurmc.org is down or your Internet connection is not working)");
                 return new List<ServerVersion>();
             }
             catch (Exception e)
             {
                 ErrorLogger.Append(e);
                 Console.WriteLine(
-                    "Error parsing response from serverjars.com! If you see this report it to a Fork maintainer.");
+                    "Error parsing response from api.purpurmc.org! If you see this report it to a Fork maintainer.");
                 return new List<ServerVersion>();
             }
         }
 
-        dynamic dyn = JsonConvert.DeserializeObject(json);
-        dyn = dyn.response;
+        PurpurVersions purpurVersions = JsonConvert.DeserializeObject<PurpurVersions>(json);
         List<ServerVersion> result = new();
-        foreach (dynamic version in dyn)
+        
+        foreach (dynamic version in purpurVersions.versions)
         {
             ServerVersion serverVersion = new();
             serverVersion.Type = ServerVersion.VersionType.Purpur;
-            serverVersion.Version = version.version;
-            serverVersion.JarLink = "https://serverjars.com/api/fetchJar/servers/purpur/" + version.version;
+            serverVersion.Version = version;
+            serverVersion.JarLink = $"https://api.purpurmc.org/v2/purpur/{version}/latest/download";
             result.Add(serverVersion);
         }
 
+        result.Reverse();
         return result;
+    }
+
+    private record PurpurVersions
+    {
+        public string project { get; set; }
+        public string[] versions { get; set; }
     }
 }
