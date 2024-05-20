a26963b15b89349ed3b4a10b3b61323665630ad0
ChristianKern97 <ikerneii97@gmail.com>
2024-05-20 11:26:22 +0200

    Updated License and publish config

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Fork")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
